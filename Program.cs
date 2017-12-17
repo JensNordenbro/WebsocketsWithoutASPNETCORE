8e351dd783a69b1475b626740f0bdfbd88746dba
Jens Nordenbro <jens.nordenbro@gmail.com>
2017-12-17 21:14:56 +0100

    A start working at both WSL Ubuntu and Windows 10

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace plainws
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task t = Task.Run( async () => {
+                HttpListener httpListener = new HttpListener();
+                httpListener.Prefixes.Add("http://localhost:7777/");
+                httpListener.Start();
+                Console.WriteLine("listener started");
+
+                HttpListenerContext context = await httpListener.GetContextAsync();
+                Console.WriteLine("context received");
+                if (context.Request.IsWebSocketRequest)
+                {
+                    HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+                    WebSocket webSocket = webSocketContext.WebSocket;
+                    while (webSocket.State == WebSocketState.Open)
+                    {
+                        //await webSocket.SendAsync( );
+                    }
+                }
+            });
+
+            t.Wait();
+            
+        }
+    }
+}
